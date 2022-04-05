fa3acb94217749cfad169df6e833cd83be0d2a32
eduardoefft <eduardo.effting@euax.com.br>
2022-04-05 13:52:23 -0300

    Adicionando JS

---
+++ index.js
@@ -0,0 +1,7 @@
+function teste() {
+    const name = document.getElementById('name').value
+    const email = document.getElementById('email').value
+    const area = document.getElementById('area').value
+    
+    return alert(`Wooooooow! \n \n Seja bem-vindo ${name} ao bootcamp da área de ${area}. \n \n Você se cadastrou com o email \n ${email}.`)
+}
